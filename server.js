09fa97cd32f56a7443f4d542a9ca4a1b08ef204a
najwanap12 <najwanap.1201@gmail.com>
2023-11-01 08:59:45 +0700

    First Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const http = require('http');
+
+const todos = [
+    { id: 1, text: 'Todo One' },
+    { id: 2, text: 'Todo Two' },
+    { id: 3, text: 'Todo Three' },
+];
+
+const server = http.createServer((req, res) => {
+   
+    const {method, url} = req;
+    let body = [];
+
+    req.on('data', chunk => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            
+            let status = 404;
+            const response = {
+                success: false,
+                result: [],
+                error: ''
+            };
+
+            if (method === 'GET' && url === '/todos') {
+
+                status = 200;
+                response.success = true;
+                response.result = todos;
+
+            } else if (method === 'POST' && url === '/todos') {
+
+                const { id, text } = JSON.parse(body);
+        
+                if (!id || !text) {
+                    status = 400;
+                    response.error = 'Please add id and text';
+                } else {
+                    todos.push({id, text});
+                    status = 201;
+                    response.success = true;
+                    response.result = todos;
+                }
+            }
+        
+
+        res.writeHead(status, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+        });
+
+        res.end(JSON.stringify(response));
+    });
+});
+
+const port = 5050;
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+server.listen(port, () => {
+    console.log("Hore konek..." + port);
+});
